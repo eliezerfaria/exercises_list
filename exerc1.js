6cbce7a09efab1478bc5e8610c60881af99c0da3
eliezerfaria <eliezerfaria@gmail.com>
2025-08-25 15:18:04 -0300

    First Commit

---
+++ exerc1.js
@@ -0,0 +1,5 @@
+console.log("Try programiz.pro");
+var employee_salary, newsalary;
+employee_salary = prompt ("Enter actual emploee salary, please ")
+newsalary = employee_salary * 1.25;
+console.log(newsalary)
---
+++ exerc2.js
@@ -0,0 +1,6 @@
+console.log("Try programiz.pro");
+var employee_salary, newsalary, percent;
+percent = prompt ("Enter the percentual tax. Ex.: 25% = type 25 ")
+employee_salary = prompt ("Enter actual emploee salary, please ")
+newsalary = employee_salary * (1.0 + percent/100);
+console.log(newsalary)
