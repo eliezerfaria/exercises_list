923fd78395c0a23a861317c4fd73d8f98b188f51
eliezerfaria <eliezerfaria@gmail.com>
2025-08-25 16:26:30 -0300

    Enviando o exercício 4

---
+++ exerc4.js
@@ -0,0 +1,10 @@
+console.log("Try programiz.pro - Exercise 4  " );
+var sacokg = 0, cat1= 0, cat2 =0;
+    sacokg = prompt ("Enter with weight of cat's food, please ")
+    cat1 = prompt ("Quantos gramas come o gato 1 ? ")
+    cat2 = prompt ("Quantos gramas come o gato 2 ? ")
+    console.log ("O saco de ração tem ", sacokg, "kg");
+    console.log ("O gato 1 come ", cat1 , "gramas de ração por dia");
+    console.log ("O gato 2 come ", cat2 , "gramas de ração por dia");
+    console.log ("Após 5 dias, os gatos comerão ", total1 = ((cat1 * 5) + (cat2 * 5)), "gramas");        
+    console.log ("Após 5 dias, o saco de ração terá ", (sacokg -(total1/1000) ),"kg"); 
